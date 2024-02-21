7b4fd8fb99551cbbb8aab37a4508a5aeba3644ef
Jonathan Motta <jonathan.motta44@gmail.com>
2024-02-21 10:27:10 -0300

    feat: create new tag

--- src/app.tsx
+++ src/app.tsx
@@ -15,9 +15,11 @@ import { keepPreviousData, useQuery } from '@tanstack/react-query'
 import { Pagination } from './components/pagination'
 import { useSearchParams } from 'react-router-dom'
 import { useState } from 'react'
-
+import * as Dialog from '@radix-ui/react-dialog'
+import { CreateTagForm } from './components/create-tag-form'
 export interface Tag {
   title: string
+  slug: string
   amountOfVideos: number
   id: string
 }
@@ -73,10 +75,28 @@ export function App() {
       <main className="max-w-6xl mx-auto space-y-5">
         <div className="flex items-center gap-3">
           <h1 className="text-xl font-bold">Tags</h1>
-          <Button variant="primary">
-            <Plus className="size-3" />
-            Create new
-          </Button>
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <Button variant="primary">
+                <Plus className="size-3" />
+                Create new
+              </Button>
+            </Dialog.Trigger>
+            <Dialog.Portal>
+              <Dialog.Overlay className="fixed inset-0  bg-black/70" />
+              <Dialog.Content className="fixed right-0 bottom-0 h-screen min-w-[320px] z-10 border-l border-zinc-900 p-10 bg-zinc-950 space-y-10">
+                <div className="space-y-3">
+                  <Dialog.Title className="text-xl font-bold">
+                    Create Tag
+                  </Dialog.Title>
+                  <Dialog.Description className="text-sm text-zinc-500">
+                    Tags can be used to group videos about similar concepts
+                  </Dialog.Description>
+                </div>
+                <CreateTagForm />
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
         </div>
         <div className="flex items-center justify-between">
           <div className="flex items-center">
@@ -116,7 +136,7 @@ export function App() {
                   <TableCell>
                     <div className="flex flex-col gap-0.5">
                       <span className="font-medium">{tag.title}</span>
-                      <span className="text-xs text-zinc-500">{tag.id}</span>
+                      <span className="text-xs text-zinc-500">{tag.slug}</span>
                     </div>
                   </TableCell>
                   <TableCell className="text-zinc-300">
